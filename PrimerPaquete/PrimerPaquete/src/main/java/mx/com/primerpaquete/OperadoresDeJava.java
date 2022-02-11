14eaeab9b731251ce424d0cc0b1f6d223224f337
Bernardo Monroy <papiberna.1106@gmail.com>
2022-02-10 18:02:47 -0600

    variables

--- PrimerPaquete/PrimerPaquete/src/main/java/mx/com/primerpaquete/OperadoresDeJava.java
+++ PrimerPaquete/PrimerPaquete/src/main/java/mx/com/primerpaquete/OperadoresDeJava.java
@@ -9,9 +9,9 @@ package mx.com.primerpaquete;
  * @author bernardo
  */
 public class OperadoresDeJava {
-    
+
     public static void main(String[] args) {
-        
+
         int a = 3, b = 2;
         //suma
         int resultado = a + b;
@@ -24,15 +24,15 @@ public class OperadoresDeJava {
         //multiplicacion
         resultado = a * b;
         System.out.println("resultado = " + resultado);
-        var resultado2 = 3D / b ;
+        var resultado2 = 3D / b;
         System.out.println("resultado2 = " + resultado2);
-        
-      int c=4, d=3;
-      int e = c + 5 - d;
+
+        int c = 4, d = 3;
+        int e = c + 5 - d;
         System.out.println("c=" + c);
-        c += 1 ;//a=a+1
+        c += 1;//a=a+1
         System.out.println("c = " + c);
-      
+
         c += 3;//aa=+3
         System.out.println("c = " + c);
         d -= 1;//d=d-1
@@ -44,11 +44,11 @@ public class OperadoresDeJava {
         int q = 9;
         int w = -q;
         System.out.println("w = " + w);
-        
+
         boolean r = true;
         boolean t = !r;
         System.out.println("t = " + t);
-        
+
         // operadorez de incremento e implemento
         //preincremento
         int s = 3;
@@ -69,22 +69,50 @@ public class OperadoresDeJava {
         int k = 4;
         int l = k--;
         System.out.println("k = " + k);
-        System.out.println("l = " + l);        
-        
+        System.out.println("l = " + l);
+
         //operadores de igualdad y relacionales 
-        int z = 1, x=7;
-        boolean c = (z == x);
-        System.out.println("c = " + c);
-        c = (z != b);
-        System.out.println("c = " + c);
+        int z = 7, x = 10;
+        boolean p = (z == x);
+        System.out.println("p = " + p);
+        p = (z != b);
+        System.out.println("p = " + p);
+        String cadena = "hola";
+        String cadena2 = "adios";
+        System.out.println(cadena.equals(cadena2));
+        //Operadores Renacionales 
+        boolean v = z > x;
+        System.out.println("v = " + v);
+        if (z / 2 == 0) {
+            System.out.println("nue=mero par");
+        } else {
+
+        }
+
+        System.out.println("numero impar");
+        int zzz = 12;
+        int god = 18;
+        if (zzz >= god) {
+            System.out.println("es un adulto");
+        } else {
+            System.out.println("es menor de edad y muy zzz");
+            //operadores condicionales  
+            int ya = 11;
+            int valorMinimo = 0, valorMaximo = 11;
+            boolean cierto = ya >= valorMinimo && ya <= valorMaximo;
+            System.out.println("cierto = " + cierto);
+
+            boolean vacaciones = false;
+            boolean descanso = false;
+            if (vacaciones || descanso);
+            System.out.println("el padre puede asistir al juego del hijo ");
+
+            System.out.println("padre esta ocupado");
+
+        }
+        //operadores tenario 
         
-        
-        
-        
-        
-        
-        
-        
+
     }
-    
+
 }
