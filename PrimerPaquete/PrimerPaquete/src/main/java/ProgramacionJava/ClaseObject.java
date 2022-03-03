837e879095377bfb8a92c78a5d12886865c2c708
Bernardo Monroy <papiberna.1106@gmail.com>
2022-03-03 11:14:09 -0600

    god

--- PrimerPaquete/PrimerPaquete/src/main/java/ProgramacionJava/ClaseObject.java
+++ PrimerPaquete/PrimerPaquete/src/main/java/ProgramacionJava/ClaseObject.java
@@ -8,7 +8,33 @@ public class ClaseObject {
         Empleado136 emp2 = new Empleado136("Juan", 1000);
         System.out.println("emp2 = " + emp2);
         System.out.println("Objetos iguales: " + (emp1 == emp2));
-        
+
+        compararObjetos(emp1, emp2);
+    }
+
+    private static void compararObjetos(Empleado136 emp1, Empleado136 emp2) {
+        System.out.println("emp1 = " + emp1);
+        System.out.println("emp2 = " + emp2);
+        //revicion por referencia 
+        if (emp1 == emp2) {
+            System.out.println("Los objetos tienen misma direccion de memoria: ");
+        } else {
+            System.out.println("No tienen igualdan zzz: ");
+        }
+        //revicion del metodo equals
+        if (emp1.equals(emp2)) {
+            System.out.println("Mismo contenido");
+        } else {
+            System.out.println("No es lo mismo :3");
+        }
+        //revicion del metodo hasdcode
+        if (emp1.hashCode() == emp2.hashCode()) {
+            System.out.println("Igualdad");
+            System.out.println("emp1 = " + emp1.hashCode());
+            System.out.println("emp2 = " + emp2.hashCode());
+        } else {
+            System.out.println("Desigualdad");
+        }
     }
 
 }
