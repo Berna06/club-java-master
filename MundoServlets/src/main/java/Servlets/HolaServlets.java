9a515ff5bbd28766cb62b13b694bc65fd59ac96f
Bernardo Monroy <papiberna.1106@gmail.com>
2022-04-09 08:26:55 -0600

    WEB

---
+++ MundoServlets/src/main/java/Servlets/HolaServlets.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author bernardo
+ */
+@WebServlet("/MundoServlet")
+public class HolaServlets extends HttpServlet{
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("Hola desde Servlets");
+    }
+}
