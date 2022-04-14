620aac6321c1ece2a01189ece2e5627ede2812d6
Bernardo Monroy <papiberna.1106@gmail.com>
2022-04-14 10:41:19 -0600

    WEB

--- MundoServlets/src/main/java/Servlets/formularios.java
+++ MundoServlets/src/main/java/Servlets/formularios.java
@@ -4,6 +4,8 @@
  */
 package Servlets;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
@@ -11,7 +13,78 @@ import javax.servlet.http.*;
  *
  * @author bernardo
  */
-@WebServlet ("/Servlets")
-public class formularios extends HttpServlet{
-    
+@WebServlet("/Servlets")
+public class formularios extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+        String tegnologias[] = request.getParameterValues("tegnologia");
+        String genero = request.getParameter("genero");
+        String ocupacion = request.getParameter("ocupacion");
+
+        out.print("<html>");
+        out.print("<head>");
+        out.print("<title>Resultado obtenido<title>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print("<h1>Parametros procesados<h1>");
+        out.print("<table border='1'>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Usuario");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(usuario);
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Password");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(password);
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Tegnolgia");
+        out.print("</td>");
+        out.print("<td>");
+        for (String tegnologia : tegnologias) {
+            out.print(tegnologia);
+            out.print(" / ");
+        }
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Genero");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(genero);
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("<tr>");
+        out.print("<td>");
+        out.print("Ocupacion");
+        out.print("</td>");
+        out.print("<td>");
+        out.print(ocupacion);
+        out.print("</td>");
+        out.print("</tr>");
+
+        out.print("</table>");
+        out.print("</body>");
+        out.print("</html>");
+    }
 }
